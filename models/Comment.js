035219cb1272eb6a36713fe3009a80b434f4ccc4
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-08-26 18:03:20 -0500

    integrate reply functionality with the from end

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,55 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    //use ReplySchema to validate data for a reply
+    replies: [ReplySchema],
   },
-  commentBody: {
-    type: String,
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+const ReplySchema = new Schema(
+  {
+    //set custome id to avoid confusiong with paarent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    replyBody: {
+      type: String,
+    },
+    writtenBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const Comment = model("Comment", CommentSchema);
 
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 //set up route called /api/comments/:pizzaId and use the addComment() method as a post callback
 //then set up another route for /api/comments/:pizzaId/:commentId and use the removeComment method as a delete callback
@@ -11,5 +13,7 @@ const {
 router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+//create a DELETE route to handle removeReply
+router.rout("/:PizzaId/:commentId/:replyId").delete(removeReply);
 module.exports = router;

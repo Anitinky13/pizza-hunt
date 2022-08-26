c87527342b4684b6fc66e7878614e328fffd8f58
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-08-26 18:48:44 -0500

    deploying

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,4 +1,3 @@
-//import the methods from the comment controller file
 const router = require("express").Router();
 const {
   addComment,
@@ -6,14 +5,14 @@ const {
   addReply,
   removeReply,
 } = require("../../controllers/comment-controller");
-//set up route called /api/comments/:pizzaId and use the addComment() method as a post callback
-//then set up another route for /api/comments/:pizzaId/:commentId and use the removeComment method as a delete callback
 
-//api/comments/<pizzaId>
+// /api/comments/<pizzaId>
 router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
-//create a DELETE route to handle removeReply
-router.rout("/:PizzaId/:commentId/:replyId").delete(removeReply);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
+
 module.exports = router;
